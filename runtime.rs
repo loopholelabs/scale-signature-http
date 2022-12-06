ca0ee209f5053976e0552c56c4bc4e2ce79a7aca
Daniel Phillips <daniel@loopholelabs.io>
2022-12-06 12:37:42 -0600

    re-org trait new make more sense

--- runtime.rs
+++ runtime.rs
@@ -1,14 +1,48 @@
 use http_signature::{Encode, Decode, Context, Request, Response};
 
-struct RuntimeContext {
+struct RuntimeState {
     generated: Context,
     buffer: &mut Cursor<&mut Vec<u8>>,
 }
 
-impl RuntimeContext for RuntimeContext {
+pub trait RuntimeContext {
+    fn new(self) -> Self;
+    fn read(&mut self) -> Context;
+    fn write(&self) -> Vec<u8>;
+    fn error(&self, err: std::io::Error) -> Vec<u8>;
+    fn generated(&self) -> &Context;
+}
+
+impl RuntimeContext for RuntimeState {
+    fn new(self) -> Self {
+        RuntimeContext {
+            generated: Context {
+                        request: Request {
+                            headers: HashMap::new(),
+                            method: "".to_string(),
+                            content_length: 0,
+                            protocol: "".to_string(),
+                            i_p: "".to_string(),
+                            body: Vec::new()
+                        },
+                        response: Response {
+                            headers: HashMap::new(),
+                            status_code: 0,
+                            body: Vec::new()
+                        },
+                   }
+            },
+          buffer: &mut Cursor<&mut Vec<u8>>,
+    }
+
+    fn generated(&self) -> &Context {
+        &self.generated
+    }
+
     fn read(&mut self) -> Context {
         Decode::decode(self.buffer).unwrap().unwrap()
     }
+
 
     fn write(&self) -> Vec<u8> {
         let mut cursor = Cursor::new(Vec::new());
@@ -21,30 +55,4 @@ impl RuntimeContext for RuntimeContext {
         let _ = Error::error(self, &mut cursor);
         cursor.into_inner()
     }
-}
-
-fn new() -> RuntimeContext {
-    RuntimeContext {
-        generated: Context {
-                    request: Request {
-                        headers: HashMap::new(),
-                        method: "".to_string(),
-                        content_length: 0,
-                        protocol: "".to_string(),
-                        i_p: "".to_string(),
-                        body: Vec::new()
-                    },
-                    response: Response {
-                        headers: HashMap::new(),
-                        status_code: 0,
-                        body: Vec::new()
-                    },
-           }
-      },
-    buffer: &mut Cursor<&mut Vec<u8>>,
-    }
-}
-
-fn generated(&self) -> &Context {
-    &self.generated
 }
