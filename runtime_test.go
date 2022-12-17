0a2ddc10398f66e66d219998e3c57b3cf4dc61e1
Daniel Phillips <daniel@loopholelabs.io>
2022-12-16 20:50:00 -0600

    Revert tests until harness is updated

--- runtime_test.go
+++ runtime_test.go
@@ -35,41 +35,41 @@ type TestCase struct {
 }
 
 func TestSignature(t *testing.T) {
-	passthroughModuleGo := &harness.Module{
+	passthroughModule := &harness.Module{
 		Name:      "passthrough",
-		Path:      "tests/modules/go/passthrough/passthrough.go",
+		Path:      "tests/modules/passthrough/passthrough.go",
 		Signature: "github.com/loopholelabs/scale-signature-http",
 	}
 
-	nextModuleGo := &harness.Module{
+	nextModule := &harness.Module{
 		Name:      "next",
-		Path:      "tests/modules/go/next/next.go",
+		Path:      "tests/modules/next/next.go",
 		Signature: "github.com/loopholelabs/scale-signature-http",
 	}
 
-	fileModuleGo := &harness.Module{
+	fileModule := &harness.Module{
 		Name:      "file",
-		Path:      "tests/modules/go/file/file.go",
+		Path:      "tests/modules/file/file.go",
 		Signature: "github.com/loopholelabs/scale-signature-http",
 	}
 
-	networkModuleGo := &harness.Module{
+	networkModule := &harness.Module{
 		Name:      "network",
-		Path:      "tests/modules/go/network/network.go",
+		Path:      "tests/modules/network/network.go",
 		Signature: "github.com/loopholelabs/scale-signature-http",
 	}
 
-	panicModuleGo := &harness.Module{
+	panicModule := &harness.Module{
 		Name:      "panic",
-		Path:      "tests/modules/go/panic/panic.go",
+		Path:      "tests/modules/panic/panic.go",
 		Signature: "github.com/loopholelabs/scale-signature-http",
 	}
 
-	goModules := []*harness.Module{passthroughModule, nextModule, fileModule, networkModule, panicModule}
+	modules := []*harness.Module{passthroughModule, nextModule, fileModule, networkModule, panicModule}
 
-	generatedGoModules := harness.GoSetup(t, goModules, "github.com/loopholelabs/scale-signature-http/tests/modules/go")
+	generatedModules := harness.Setup(t, modules, "github.com/loopholelabs/scale-signature-http/tests/modules")
 
-	var goTestCases = []TestCase{
+	var testCases = []TestCase{
 		{
 			Name:   "Passthrough",
 			Module: passthroughModule,
@@ -173,147 +173,10 @@ func TestSignature(t *testing.T) {
 		},
 	}
 
-	passthroughModuleRust := &harness.Module{
-		Name:      "passthrough",
-		Path:      "tests/modules/rust/passthrough.rs",
-		Signature: "scale_signature_http",
-	}
-
-	nextModuleRust := &harness.Module{
-		Name:      "next",
-		Path:      "tests/modules/rust/next.rs",
-		Signature: "scale_signature_http",
-	}
-
-	fileModuleRust := &harness.Module{
-		Name:      "file",
-		Path:      "tests/modules/rust/file.rs",
-		Signature: "scale_signature_http",
-	}
-
-	networkModuleRust := &harness.Module{
-		Name:      "network",
-		Path:      "tests/modules/rust/network.rs",
-		Signature: "scale_signature_http",
-	}
-
-	panicModuleRust := &harness.Module{
-		Name:      "panic",
-		Path:      "tests/modules/rust/panic.rs",
-		Signature: "scale_signature_http",
-	}
-
-	rustModules := []*harness.Module{passthroughModuleRust, nextModuleRust, fileModuleRust, networkModuleRust, panicModuleRust}
-
-	generatedRustModules := harness.RustSetup(t, rustModules, "github.com/loopholelabs/scale-signature-http/tests/modules/rust")
-
-	var rustTestCases = []TestCase{
-		{
-			Name:   "Passthrough",
-			Module: passthroughModule,
-			Run: func(scaleFunc *scalefunc.ScaleFunc, t *testing.T) {
-				r, err := runtime.New(context.Background(), New(), []*scalefunc.ScaleFunc{scaleFunc})
-				require.NoError(t, err)
-
-				i, err := r.Instance(nil)
-				require.NoError(t, err)
-
-				i.Context().Response().SetBody("Test Data")
-
-				err = i.Run(context.Background())
-				assert.NoError(t, err)
-
-				assert.Equal(t, []byte("Test Data"), i.Context().Response().Body())
-			},
-		},
-		{
-			Name:   "Next",
-			Module: nextModule,
-			Run: func(scaleFunc *scalefunc.ScaleFunc, t *testing.T) {
-				next := func(ctx *Context) (*Context, error) {
-					ctx.Response().SetBody("Hello, World!")
-					return ctx, nil
-				}
-
-				r, err := runtime.New(context.Background(), New(), []*scalefunc.ScaleFunc{scaleFunc})
-				require.NoError(t, err)
-
-				i, err := r.Instance(next)
-				require.NoError(t, err)
-
-				i.Context().Response().SetBody("Test Data")
-
-				err = i.Run(context.Background())
-				assert.NoError(t, err)
-
-				assert.Equal(t, []byte("Hello, World!"), i.Context().Response().Body())
-			},
-		},
-		{
-			Name:   "NextError",
-			Module: nextModule,
-			Run: func(scaleFunc *scalefunc.ScaleFunc, t *testing.T) {
-				next := func(ctx *Context) (*Context, error) {
-					return nil, errors.New("next error")
-				}
-
-				r, err := runtime.New(context.Background(), New(), []*scalefunc.ScaleFunc{scaleFunc})
-				require.NoError(t, err)
-
-				i, err := r.Instance(next)
-				require.NoError(t, err)
-
-				err = i.Run(context.Background())
-				require.ErrorIs(t, err, errors.New("next error"))
-			},
-		},
-		{
-			Name:   "File",
-			Module: fileModule,
-			Run: func(scaleFunc *scalefunc.ScaleFunc, t *testing.T) {
-				r, err := runtime.New(context.Background(), New(), []*scalefunc.ScaleFunc{scaleFunc})
-				require.NoError(t, err)
-
-				i, err := r.Instance(nil)
-				require.NoError(t, err)
-
-				err = i.Run(context.Background())
-				require.Error(t, err)
-			},
-		},
-		{
-			Name:   "Network",
-			Module: networkModule,
-			Run: func(scaleFunc *scalefunc.ScaleFunc, t *testing.T) {
-				r, err := runtime.New(context.Background(), New(), []*scalefunc.ScaleFunc{scaleFunc})
-				require.NoError(t, err)
-
-				i, err := r.Instance(nil)
-				require.NoError(t, err)
-
-				err = i.Run(context.Background())
-				require.Error(t, err)
-			},
-		},
-		{
-			Name:   "Panic",
-			Module: panicModule,
-			Run: func(scaleFunc *scalefunc.ScaleFunc, t *testing.T) {
-				r, err := runtime.New(context.Background(), New(), []*scalefunc.ScaleFunc{scaleFunc})
-				require.NoError(t, err)
-
-				i, err := r.Instance(nil)
-				require.NoError(t, err)
-
-				err = i.Run(context.Background())
-				require.Error(t, err)
-			},
-		},
-	}
-	for _, testCase := range goTestCases {
+	for _, testCase := range testCases {
 		t.Run(testCase.Name, func(t *testing.T) {
 
-			module, err := os.ReadFile(generatedGoModules[testCase.Module])
+			module, err := os.ReadFile(generatedModules[testCase.Module])
 			require.NoError(t, err)
 
 			scaleFunc := &scalefunc.ScaleFunc{
@@ -321,23 +184,6 @@ func TestSignature(t *testing.T) {
 				Name:      "TestName",
 				Signature: "http@v0.1.1",
 				Language:  "go",
-				Function:  module,
-			}
-			testCase.Run(scaleFunc, t)
-		})
-	}
-
-	for _, testCase := range rustTestCases {
-		t.Run(testCase.Name, func(t *testing.T) {
-
-			module, err := os.ReadFile(generatedRustModules[testCase.Module])
-			require.NoError(t, err)
-
-			scaleFunc := &scalefunc.ScaleFunc{
-				Version:   "TestVersion",
-				Name:      "TestName",
-				Signature: "http@v0.1.1",
-				Language:  "rust",
 				Function:  module,
 			}
 			testCase.Run(scaleFunc, t)
