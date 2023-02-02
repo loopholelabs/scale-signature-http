d5eec10e3f55fb9431eabb74be5d56e05bb8aff5
Shivansh Vij <shivanshvij@outlook.com>
2023-02-02 01:03:35 -0500

    Removing generated functions for runtime implementations (#16)

--- context.rs
+++ context.rs
@@ -17,10 +17,12 @@
 use crate::http_signature::{HttpContext, HttpRequest, HttpResponse};
 use std::collections::HashMap;
 
+#[cfg(target_arch = "wasm32")]
 pub struct Context {
     pub(crate) generated: HttpContext,
 }
 
+#[cfg(target_arch = "wasm32")]
 pub fn new() -> Context {
     Context {
         generated: HttpContext {
@@ -41,3 +43,27 @@ pub fn new() -> Context {
         },
     }
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+pub type Context = HttpContext;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn new() -> Context {
+    Context {
+        request: HttpRequest {
+            headers: HashMap::new(),
+            uri: "".to_string(),
+            method: "".to_string(),
+            content_length: 0,
+            protocol: "".to_string(),
+            ip: "".to_string(),
+            body: Vec::new(),
+        },
+        response: HttpResponse {
+            headers: HashMap::new(),
+            status_code: 0,
+            body: Vec::new(),
+        },
+    }
+}
+
