93549eb799168b0d65d8774f4a50b76068e57d1a
Shivansh Vij <shivanshvij@outlook.com>
2023-02-01 20:03:55 -0500

    Staging Release v0.1.5 (#11)
    * Pushing partial changes
    
    * Updating scale signature for rust to properly passthrough errors
    
    * Updating rust interfaces for requests and responses
    
    * Cleaning up go dependencies
    
    * Making generated parameters crate-public only
    
    * Updating Rust, Golang, and Typescript semantics to match one another

--- go.mod
+++ go.mod
@@ -4,17 +4,11 @@ go 1.18
 
 require (
 	github.com/loopholelabs/polyglot-go v0.5.1
-	github.com/loopholelabs/scale v0.1.5
-	github.com/loopholelabs/scale-signature v0.1.3
-	github.com/loopholelabs/scalefile v0.1.0
-	github.com/stretchr/testify v1.8.1
+	github.com/loopholelabs/scale-signature v0.1.5
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/tetratelabs/wazero v1.0.0-pre.4 // indirect
+	github.com/loopholelabs/scalefile v0.1.4 // indirect
 	github.com/yalue/merged_fs v1.2.2 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- request.rs
+++ request.rs
@@ -1,83 +1,79 @@
 /*
-	Copyright 2022 Loophole Labs
+    Copyright 2022 Loophole Labs
 
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-		   http://www.apache.org/licenses/LICENSE-2.0
+           http://www.apache.org/licenses/LICENSE-2.0
 
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 
-#![allow(unused_variables)]
 use crate::http_signature::{HttpRequest, HttpStringList};
-use std::string::String;
 use std::collections::HashMap;
+use std::string::String;
 
-pub trait Request {
-    fn request(self) -> Self;
-    fn method(&mut self) -> String;
-    fn set_method(&mut self, method: String) -> &mut Self;
-    fn uri(&mut self) -> String;
-    fn remote_ip(&mut self) -> String;
-    fn body(&mut self) -> Vec<u8>;
-    fn set_body(&mut self, body: String) -> &mut Self;
-    fn set_body_bytes(&mut self, bytes: Vec<u8>) -> &mut Self;
-    fn headers(&self) -> &HashMap<String, HttpStringList>;
-    fn get_headers(&self, key: &String) -> Option<&HttpStringList>;
-    fn set_headers(&mut self, key: String, value: Vec<String>);
-}
+pub type Request = HttpRequest;
 
-impl Request for HttpRequest {
-    fn request(self) -> Self {
-        self
-    }
-
-    fn method(&mut self) -> String {
+impl Request {
+    pub fn method(&mut self) -> String {
         self.method.clone()
     }
 
-    fn set_method(&mut self, method: String) -> &mut Self {
+    pub fn set_method(&mut self, method: String) -> &mut Self {
         self.method = method;
         self
     }
 
-    fn uri(&mut self) -> String {
+    pub fn uri(&mut self) -> String {
         self.uri.clone()
     }
 
-    fn remote_ip(&mut self) -> String {
-        self.ip.clone()
+    pub fn set_uri(&mut self, uri: String) -> &mut Self {
+        self.uri = uri;
+        self
     }
 
-    fn body(&mut self) -> Vec<u8> {
+    pub fn body(&mut self) -> Vec<u8> {
         self.body.clone()
     }
 
-    fn set_body(&mut self, body: String) -> &mut Self {
-        self.body = body.as_bytes().to_vec();
-        self
+    pub fn set_body(&mut self, body: String) -> &mut Self {
+        return self.set_body_bytes(body.as_bytes().to_vec());
     }
 
-    fn set_body_bytes(&mut self, bytes: Vec<u8>) -> &mut Self {
+    pub fn set_body_bytes(&mut self, bytes: Vec<u8>) -> &mut Self {
         self.body = bytes;
+        self.content_length = self.body.len() as i64;
         self
     }
 
-    fn headers(&self) -> &HashMap<String, HttpStringList> {
+    pub fn content_length(&mut self) -> i64 {
+        self.content_length
+    }
+
+    pub fn remote_ip(&mut self) -> String {
+        self.ip.clone()
+    }
+
+    pub fn protocol(&mut self) -> String {
+        self.protocol.clone()
+    }
+
+    pub fn headers(&self) -> &HashMap<String, HttpStringList> {
         &self.headers
     }
 
-    fn get_headers(&self, key: &String) -> Option<&HttpStringList>{
+    pub fn get_headers(&self, key: &String) -> Option<&HttpStringList> {
         self.headers.get(key)
     }
 
-    fn set_headers(&mut self, key: String, value: Vec<String>) {
-        self.headers.insert(key,  HttpStringList{ value: value });
+    pub fn set_headers(&mut self, key: String, value: Vec<String>) {
+        self.headers.insert(key, HttpStringList { value });
     }
 }
